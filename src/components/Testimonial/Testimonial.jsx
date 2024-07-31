dd5b9d01951998b4ddb94b9ce44a0eff43e2ee71
Ghanishtha12 <kapoorghanishtha2005@gmail.com>
2024-07-31 15:38:35 +0530

    completed

--- src/components/Testimonial/Testimonial.jsx
+++ src/components/Testimonial/Testimonial.jsx
@@ -10,14 +10,14 @@ function Testimonial() {
                 <div className="container px-5 py-10 mx-auto">
                     <h1 className=' text-center text-3xl font-bold text-black' style={{ color: mode === 'dark' ? 'white' : '' }}>
                         Testimonial</h1>
-                    <h2 className=' text-center text-2xl font-semibold mb-10' style={{ color: mode === 'dark' ? 'white' : '' }}>What our
-                        <span className=' text-orange-400 font-bold underline decoration-wavy'> customers</span> are saying</h2>
+                    {/* <h2 className=' text-center text-2xl font-semibold mb-10' style={{ color: mode === 'dark' ? 'white' : '' }}>What our
+                        <span className=' text-orange-400 font-bold underline decoration-wavy'> customers</span> are saying</h2> */}
                     <div className="flex flex-wrap -m-4">
                         <div className="lg:w-1/3 lg:mb-0 mb-6 p-4">
                             <div className="h-full text-center">
-                                <img alt="testimonial" className="w-20 h-20 mb-8 object-cover object-center rounded-full inline-block
-                                 border-2 border-gray-200 bg-gray-100" src="https://cdn-icons-png.flaticon.com/128/4333/4333609.png" />
-                                <p style={{ color: mode === 'dark' ? 'white' : '' }} className="leading-relaxed">My experience with the customer service team here was
+                                {/* <img alt="testimonial" className="w-20 h-20 mb-8 object-cover object-center rounded-full inline-block
+                                 border-2 border-gray-200 bg-gray-100" src="https://cdn-icons-png.flaticon.com/128/4333/4333609.png" /> */}
+                                {/* <p style={{ color: mode === 'dark' ? 'white' : '' }} className="leading-relaxed">My experience with the customer service team here was
                                     nothing short of exceptional. I encountered a minor issue with my order and reached out to their support team for assistance.They
                                     responded promptly and provided clear, helpful solutions to resolve my issue. Their commitment to customer satisfaction was evident
                                     throughout the process. It's refreshing to receive such attentive and effective service, and it has certainly earned my loyalty
@@ -25,12 +25,12 @@ function Testimonial() {
                                 <span className="inline-block h-1 w-10 rounded bg-orange-400 mt-6 mb-4" />
                                 <h2 style={{ color: mode === 'dark' ? '#ff4162' : '' }} className="text-gray-900 font-medium title-font
                                  tracking-wider text-sm uppercase underline">Krish Oberoi</h2>
-                                <p style={{ color: mode === 'dark' ? 'white' : '' }} className="text-gray-500 italic">Mumbai, India</p>
+                                <p style={{ color: mode === 'dark' ? 'white' : '' }} className="text-gray-500 italic">Mumbai, India</p> */}
                             </div>
                         </div>
                         <div className="lg:w-1/3 lg:mb-0 mb-6 p-4">
                             <div className="h-full text-center">
-                                <img alt="testimonial" className="w-20 h-20 mb-8 object-cover object-center rounded-full inline-block
+                                {/* <img alt="testimonial" className="w-20 h-20 mb-8 object-cover object-center rounded-full inline-block
                                  border-2 border-gray-200 bg-gray-100" src="https://cdn-icons-png.flaticon.com/128/2763/2763444.png" />
                                 <p style={{ color: mode === 'dark' ? 'white' : '' }} className="leading-relaxed">Shopping on this site has been a delightful experience
                                     from start to finish. The website itself is user-friendly, with an intuitive design that made it easy for me to navigate and find
@@ -40,12 +40,12 @@ function Testimonial() {
                                 <span className="inline-block h-1 w-10 rounded bg-orange-400 mt-6 mb-4" />
                                 <h2 style={{ color: mode === 'dark' ? '#ff4162' : '' }} className="text-gray-900 font-medium title-font 
                                 tracking-wider text-sm uppercase underline">Zoya Khan</h2>
-                                <p style={{ color: mode === 'dark' ? 'white' : '' }} className="text-gray-500 italic">Delhi, India</p>
+                                <p style={{ color: mode === 'dark' ? 'white' : '' }} className="text-gray-500 italic">Delhi, India</p> */}
                             </div>
                         </div>
                         <div className="lg:w-1/3 lg:mb-0 p-4">
                             <div className="h-full text-center">
-                                <img alt="testimonial" className="w-20 h-20 mb-8 object-cover object-center rounded-full inline-block
+                                {/* <img alt="testimonial" className="w-20 h-20 mb-8 object-cover object-center rounded-full inline-block
                                  border-2 border-gray-200 bg-gray-100" src="https://cdn-icons-png.flaticon.com/128/2922/2922510.png" />
                                 <p style={{ color: mode === 'dark' ? 'white' : '' }} className="leading-relaxed">The product range available on this site is truly 
                                     impressive. I was amazed by the extensive selection of items across various categories, which made it easy for me to find exactly 
@@ -55,7 +55,7 @@ function Testimonial() {
                                 <span className="inline-block h-1 w-10 rounded bg-orange-400 mt-6 mb-4" />
                                 <h2 style={{ color: mode === 'dark' ? '#ff4162' : '' }} className="text-gray-900 font-medium title-font
                                  tracking-wider text-sm uppercase underline">Aditya Mahajan</h2>
-                                <p style={{ color: mode === 'dark' ? 'white' : '' }} className="text-gray-500 italic">Punjab, India</p>
+                                <p style={{ color: mode === 'dark' ? 'white' : '' }} className="text-gray-500 italic">Punjab, India</p> */}
                             </div>
                         </div>
                     </div>
