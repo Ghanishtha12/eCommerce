e94153be1ce101331ff9466a6f6b1b86e403c938
Ghanishtha12 <kapoorghanishtha2005@gmail.com>
2025-07-28 23:13:24 +0530

    ok

--- src/components/heroSection/HeroSection.jsx
+++ src/components/heroSection/HeroSection.jsx
@@ -1,14 +1,10 @@
- import React, { useState } from 'react';
+import React, { useState } from 'react';
+import clothing from '../assets/clothing.png';
+import offer from '../assets/offer.png';
+import homeDeco from '../assets/homeDeco.png';
 
 function HeroSection() {
-
-
-
-  const images = [
-    'src\\assets\\clothing.png',
-    'src\\assets\\offer.png',
-    'src\\assets\\homeDeco.png',
-  ];
+  const images = [clothing, offer, homeDeco];
 
   const [currentSlide, setCurrentSlide] = useState(0);
 
@@ -22,20 +18,35 @@ function HeroSection() {
 
   return (
     <div className="w-full h-[500px] relative flex justify-center items-center overflow-hidden">
-      <div className="w-full h-full flex transition-transform duration-700 ease-in-out" style={{ transform: `translateX(-${currentSlide * 100}%)` }}>
+      <div
+        className="w-full h-full flex transition-transform duration-700 ease-in-out"
+        style={{ transform: `translateX(-${currentSlide * 100}%)` }}
+      >
         {images.map((src, index) => (
           <div key={index} className="w-full h-full flex-shrink-0">
-            <img className="w-full h-full object-cover" src={src} alt={`Slide ${index + 1}`} />
+            <img
+              className="w-full h-full object-cover"
+              src={src}
+              alt={`Slide ${index + 1}`}
+            />
           </div>
         ))}
       </div>
+
       {/* Navigation Arrows */}
-      <button onClick={prevSlide} className="absolute left-0 z-10 p-2 m-2 bg-gray-800 text-white rounded-full hover:bg-gray-700">
+      <button
+        onClick={prevSlide}
+        className="absolute left-0 z-10 p-2 m-2 bg-gray-800 text-white rounded-full hover:bg-gray-700"
+      >
         &#10094;
       </button>
-      <button onClick={nextSlide} className="absolute right-0 z-10 p-2 m-2 bg-gray-800 text-white rounded-full hover:bg-gray-700">
+      <button
+        onClick={nextSlide}
+        className="absolute right-0 z-10 p-2 m-2 bg-gray-800 text-white rounded-full hover:bg-gray-700"
+      >
         &#10095;
       </button>
+
       {/* Dots Navigation */}
       <div className="absolute bottom-4 flex space-x-2">
         {images.map((_, index) => (
@@ -51,6 +62,3 @@ function HeroSection() {
 }
 
 export default HeroSection;
-
-
-
