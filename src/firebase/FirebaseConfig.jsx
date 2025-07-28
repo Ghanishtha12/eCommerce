0a55576f6cdea020e211053fc59bf98f87b3a038
Ghanishtha12 <kapoorghanishtha2005@gmail.com>
2025-07-28 23:02:58 +0530

    ok

--- src/firebase/FirebaseConfig.jsx
+++ src/firebase/FirebaseConfig.jsx
@@ -5,14 +5,16 @@ import { getAuth } from "firebase/auth";
 
 
 // Your web app's Firebase configuration
+// ...existing code...
 const firebaseConfig = {
-  apiKey: "AIzaSyBLSqA32Difhqav1Swpk4O7NRLg2KibrUI",
-  authDomain: "myfirstapp-9721d.firebaseapp.com",
-  projectId: "myfirstapp-9721d",
-  storageBucket: "myfirstapp-9721d.appspot.com",
-  messagingSenderId: "356516939116",
-  appId: "1:356516939116:web:84c9ef2ba65888fb019f49"
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,  
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID
 };
+// ...existing code...
 // 5267318187975449
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
